5fbae14e3250ff7fbb6136b3145b9be9286b81c8
SpiritedHunter <pamionj@gmail.com>
2026-01-22 18:33:45 -0300

    Configura WebDriver para correr en local y CI

--- selenium-ui-tests/src/test/java/base/BaseTest.java
+++ selenium-ui-tests/src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,8 +14,30 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+
+        /*
+         * Si estamos en CI (GitHub Actions, server, etc),
+         * Chrome debe correr en modo headless
+         */
+        String isCI = System.getenv("CI");
+
+        if (isCI != null && isCI.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
+
+        // Solo maximizar si NO estamos en CI
+        if (isCI == null) {
+            driver.manage().window().maximize();
+        }
+
         driver.get("https://www.saucedemo.com");
     }
 
